22da2879239b114d94e380449d18db6a113cf1ce
vignerd <omerbcalik@gmail.com>
2021-03-26 23:13:54 +0300

    fixx

--- index.js
+++ index.js
@@ -105,5 +105,5 @@ app.delete("/api/persons/:id", (request, response, next) => {
 })
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3001")
+  console.log(`Server is running on port ${PORT}`)
 })
